fbc88bb0e794245cc6d4157cc5d0f7a1d06e26ea
Obakeng Mosadi <mosadiobakeng7@gmail.com>
2020-09-03 21:28:29 +0200

    Adding index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-8 text-center">
+            <h1>Plannr</h1>
+        </div>
+    </div>
+</div>
+@endsection
