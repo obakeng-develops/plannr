a53a83fe8387612a80fcaf9f4bd4bd3cf94c4a07
Obakeng Mosadi <mosadiobakeng7@gmail.com>
2020-09-03 21:41:48 +0200

    added font to overall site and select dropdown

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,10 +2,21 @@
 
 @section('content')
 <div class="container-fluid">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center mt-5">
         <div class="col-md-8 text-center">
             <h1 class="display-3">Plannr</h1>
             <p class="lead text-muted">What are you planning to do?</p>
+            <form>
+                <div class="form-group justify-content-center d-flex">
+                <select class="form-control form-control-lg w-50">
+                    <option>Night out</option>
+                    <option>Vacation</option>
+                    <option>Solocation</option>
+                    <option>Day out</option>
+                </select>
+                <button class="btn btn-lg bg-success ml-3 text-white">Start planning</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
