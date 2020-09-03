6feec35c6db659bcfe774cdad6bf0716cdef5b8f
Obakeng Mosadi <mosadiobakeng7@gmail.com>
2020-09-03 21:28:49 +0200

    Index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,8 @@
 <div class="container-fluid">
     <div class="row justify-content-center">
         <div class="col-md-8 text-center">
-            <h1>Plannr</h1>
+            <h1 class="display-3">Plannr</h1>
+            <p class="lead text-muted">What are you planning to do?</p>
         </div>
     </div>
 </div>
