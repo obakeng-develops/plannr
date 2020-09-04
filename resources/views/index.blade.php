604732a6ff203acfc61632f77dc8013a2b1966b0
Obakeng Mosadi <mosadiobakeng7@gmail.com>
2020-09-04 20:43:13 +0200

    Removed two options from the plan types

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,13 +8,11 @@
             <p class="lead text-muted">What are you planning to do?</p>
             <form>
                 <div class="form-group justify-content-center d-flex">
-                <select class="form-control form-control-lg w-50">
+                <select class="form-control form-control-lg w-50 rounded-0">
                     <option>Night out</option>
-                    <option>Vacation</option>
-                    <option>Solocation</option>
                     <option>Day out</option>
                 </select>
-                <button class="btn btn-lg bg-success ml-3 text-white">Start planning</button>
+                <button class="btn btn-lg bg-success ml-3 text-white rounded-0">Start planning</button>
                 </div>
             </form>
         </div>
