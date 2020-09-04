a116dc015182346fc2fe0f6d75c35f8be0b95ee5
Obakeng Mosadi <mosadiobakeng7@gmail.com>
2020-09-04 21:28:24 +0200

    Made style changes to Guests form

--- resources/views/guests.blade.php
+++ resources/views/guests.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container-fluid">
     <form>
-        <div class="row justify-content-center mt-5">
+        <div class="row justify-content-center">
             <div class="col-md-4">
                 <h1 class="text-center pt-3">Who are you taking with?</h1>
                 <p class="text-muted text-center">This is the last part of your journey with us.</p>
@@ -12,17 +12,20 @@
                         <div class="pt-3 pb-3 pl-3 pr-3">
                             <div class="form-group">
                                 <label>Guest 1:</label>
-                                <input type="text" class="form-control border-top-0 border-left-0 border-right-0" />
+                                <input type="text" class="form-control border-top-0 border-left-0 border-right-0" placeholder="Guest Name"/>
+                                <input type="email" class="form-control border-top-0 border-left-0 border-right-0" placeholder="Guest Email" />
                                 <p class="small text-muted pt-2">Who paid first?</p>
                             </div>
                             <div class="form-group">
                                 <label>Guest 2:</label>
-                                <input type="text" class="form-control border-top-0 border-left-0 border-right-0" />
+                                <input type="text" class="form-control border-top-0 border-left-0 border-right-0" placeholder="Guest Name"/>
+                                <input type="email" class="form-control border-top-0 border-left-0 border-right-0" placeholder="Guest Email" />
                                 <p class="small text-muted pt-2">Who paid after the first?</p>
                             </div>
                             <div class="form-group">
                                 <label>Guest 3:</label>
-                                <input type="text" class="form-control border-top-0 border-left-0 border-right-0" />
+                                <input type="text" class="form-control border-top-0 border-left-0 border-right-0" placeholder="Guest Name"/>
+                                <input type="email" class="form-control border-top-0 border-left-0 border-right-0" placeholder="Guest Email" />
                                 <p class="small text-muted pt-2">Who paid last?</p>
                             </div>
                             <button class="btn btn-lg mt-3 btn-danger" type="submit">And you're done</button>
