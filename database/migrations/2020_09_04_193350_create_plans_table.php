42311ae659ee77392243321a5d0fec7cc135ff13
Obakeng Mosadi <mosadiobakeng7@gmail.com>
2020-09-04 21:46:04 +0200

    Created Plan, Activity and Guest migrations & models

---
+++ database/migrations/2020_09_04_193350_create_plans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('enum', ['day_out', 'night_out']);
+            $table->string('plan_name');
+            $table->string('country');
+            $table->decimal('budget', 8, 2);
+            $table->integer('duration');
+            $table->string('number_of_people');
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+}
