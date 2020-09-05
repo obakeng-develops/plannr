50f3aaaed8704903df15e631d155db104b25d24a
Obakeng Mosadi <mosadiobakeng7@gmail.com>
2020-09-05 10:09:16 +0200

    Created Plan, Guest and Activity controllers then created a web route to add a plan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,13 +6,13 @@
         <div class="col-md-8 text-center">
             <h1 class="display-3">Plannr</h1>
             <p class="lead text-muted">What are you planning to do?</p>
-            <form>
+            <form method="POST" action="{{ route('you') }}">
                 <div class="form-group justify-content-center d-flex">
                 <select class="form-control form-control-lg w-50 rounded-0">
-                    <option>Night out</option>
-                    <option>Day out</option>
+                    <option name="night_out">Night out</option>
+                    <option name="day_out">Day out</option>
                 </select>
-                <button class="btn btn-lg bg-success ml-3 text-white rounded-0">Start planning</button>
+                <button class="btn btn-lg bg-success ml-3 text-white rounded-0" type="submit">Start planning</button>
                 </div>
             </form>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Route::get('/activities', 'PagesController@activities')->name('activities');
 Route::get('/guests', 'PagesController@guests')->name('guests');
 Route::get('/itinerary', 'PagesController@itinerary')->name('finish');
 
+Route::post('/whoareyou/{plan}', 'PlanController@addPlan')->name('add_plan');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
